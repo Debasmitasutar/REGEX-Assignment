82cd3490f8186a94b2d7232a06518e79e50aadb9
Debasmitasutar <debasmita12.ds@gmail.com>
2023-07-18 16:53:25 +0530

    RegularExpression Program

--- RegularExpression.java
+++ RegularExpression.java
@@ -2,9 +2,11 @@ package Regex;
 
 public class RegularExpression {
     public static void main(String[] args) {
-        System.out.println(firstName("Cap"));
+       // System.out.println(firstName("Cap"));
+        System.out.println(lastName("Cap"));
     }
-    public static boolean firstName(String firstName){
-        return firstName.matches("[A-Z][a-z]*");
+   // public static boolean firstName(String firstName){
+   public static boolean lastName(String lastName){
+        return lastName.matches("[A-Z]+(['-][a-zA-z]+)*");
     }
 }
