00f65b3490a3db41c4201ab4fd6606c21dbff66a
Debasmitasutar <debasmita12.ds@gmail.com>
2023-07-18 17:37:09 +0530

    RegularExpression Program

--- RegularExpression.java
+++ RegularExpression.java
@@ -2,11 +2,19 @@ package Regex;
 
 public class RegularExpression {
     public static void main(String[] args) {
-       // System.out.println(firstName("Cap"));
-        System.out.println(lastName("Cap"));
+       // System.out.println(firstName("Debasmita"));
+        //System.out.println(lastName("Sutar"));
+        System.out.println(mail("abc.xyz@bl.co.in"));
     }
+
+
    // public static boolean firstName(String firstName){
-   public static boolean lastName(String lastName){
-        return lastName.matches("[A-Z]+(['-][a-zA-z]+)*");
-    }
+   //public static boolean lastName(String lastName){
+       // return lastName.matches("[A-Z]+(['-][a-zA-z]+)*");
+    //}
+    //Validating email
+        public static boolean mail(String mail){
+            return mail.matches("[a-zA-Z0-9]+[._+-]?[a-zA-Z0-9]+@[a-zA-Z0-9]+.[a-zA-Z]*[.]*[a-zA-Z]*");
+       }
+
 }
