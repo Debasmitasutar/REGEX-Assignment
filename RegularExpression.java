bb06021f515073e52e4f94f8f736cf2fe071b884
Debasmitasutar <debasmita12.ds@gmail.com>
2023-07-18 20:11:21 +0530

    RegularExpression Program

--- RegularExpression.java
+++ RegularExpression.java
@@ -4,7 +4,8 @@ public class RegularExpression {
     public static void main(String[] args) {
        // System.out.println(firstName("Debasmita"));
         //System.out.println(lastName("Sutar"));
-        System.out.println(mail("abc.xyz@bl.co.in"));
+        //System.out.println(mail("abc.xyz@bl.co.in"));
+        System.out.println(Mobileformat("91 9937839977"));
     }
 
 
@@ -13,8 +14,14 @@ public class RegularExpression {
        // return lastName.matches("[A-Z]+(['-][a-zA-z]+)*");
     //}
     //Validating email
-        public static boolean mail(String mail){
-            return mail.matches("[a-zA-Z0-9]+[._+-]?[a-zA-Z0-9]+@[a-zA-Z0-9]+.[a-zA-Z]*[.]*[a-zA-Z]*");
-       }
+       // public static boolean mail(String mail){
+           // return mail.matches("[a-zA-Z0-9]+[._+-]?[a-zA-Z0-9]+@[a-zA-Z0-9]+.[a-zA-Z]*[.]*[a-zA-Z]*");
+      // }
+    //Mobile format
+   public static boolean Mobileformat(String Mobileformat) {
+        return Mobileformat.matches("[91]+[ ]+[1-9]{1}[0-9]{9}");
+
+   }
+
 
 }
